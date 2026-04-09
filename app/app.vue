5c94ecd61b0c15515a62da4d0ad1907b3b28384e
Forest <wilsonforest99@gmail.com>
2026-04-09 10:44:18 -0400

    a

---
+++ app/app.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <img src="./Untitled.png" alt="No" />
+  </div>
+</template>
+
+<style>
+body {
+  background-color: black;
+}
+img {
+  width: 100%;
+}
+</style>
